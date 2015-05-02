f5f15b26248c9d435d36621b284d24407df64035
Priyankar Nandi <priyankar.143@gmail.com>
2015-05-02 17:13:55 -0400

    Fecth 5000 tweets

--- fetchTweet.py
+++ fetchTweet.py
@@ -5,7 +5,7 @@ import numpy as np
 from scipy.io import loadmat
 
 num_of_features = 7
-num_of_topics = 10
+num_of_topics = 1
 auth = twitter.oauth.OAuth(OAUTH_TOKEN, OAUTH_TOKEN_SECRET,
                            CONSUMER_KEY, CONSUMER_SECRET)
 
@@ -24,8 +24,8 @@ for i in range(num_of_topics):
     topics.append(name)
 
 outfile = open("test.txt", "w")
-total_count = 500
-count_per_search = 100
+total_count = 1
+count_per_search = 1
 topic_counter = 0
 for topic in topics:
     topic_counter += 1
@@ -37,8 +37,12 @@ for topic in topics:
         search_results = twitter_api.search.tweets(q=topic, count=count_per_search, max_id=max_id)
         statuses = search_results["statuses"]
         search_results_len = len(statuses)
+        # per tweet processing
         while row_num < count_per_search:
             status = statuses[row_num]
+            print status
+            resp = json.dumps(status, indent=4)
+            print resp
             text = status["text"]
             # Message based features
             length_tweet = len(text)
@@ -49,6 +53,26 @@ for topic in topics:
             max_id = status["id"]
             num_swear_words = CommonUtil.count_swear_words(text)
             num_at_emotions = text.count("@")
+
+            # Source based Features
+            user_features = status["user"]
+            num_followers = user_features["followers_count"]
+            num_friends = user_features["friends_count"]
+            is_verified = user_features["verified"]
+            if is_verified:
+                is_verified = 1
+            else:
+                is_verified = 0
+            len_desc = len(user_features["description"])
+            len_screen_name = len(user_features["screen_name"])
+            user_url = user_features["url"]
+            print "user_url", user_url
+            has_url = 0
+            if user_url:
+                has_url = 1
+            user_creation_date = user_features["created_at"]
+            print user_creation_date
+            # Create tweet characteristics to write to file
             tweet_str = str(length_tweet) + "|" + str(num_words) + "|" + str(num_unique_chars) + "|" \
                         + str(num_hashtags) + "|" + str(retweet_cnt) + "|" + str(num_swear_words) + "|" \
                         + str(num_at_emotions)
