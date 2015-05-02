09dd327ac51a7d96f970f7dd270f2227f4a516db
Priyankar Nandi <priyankar.143@gmail.com>
2015-05-02 15:59:27 -0400

    Fecth 5000 tweets

--- fetchTweet.py
+++ fetchTweet.py
@@ -24,31 +24,42 @@ for i in range(num_of_topics):
     topics.append(name)
 
 outfile = open("test.txt", "w")
-count = 101
+total_count = 500
+count_per_search = 100
+topic_counter = 0
 for topic in topics:
-    search_results = twitter_api.search.tweets(q=topic, count=count)
-    statuses = search_results["statuses"]
-    print len(statuses)
-    row_num = 0
-    while row_num < count:
-        status = statuses[row_num]
-        text = status["text"]
-        # Message based features
-        length_tweet = len(text)
-        num_words = len(text.split())
-        num_unique_chars = CommonUtil.count_unique_chars(text)
-        num_hashtags = text.count("#")
-        retweet_cnt = status["retweet_count"]
-        num_swear_words = CommonUtil.count_swear_words(text)
-        num_at_emotions = text.count("@")
-        tweet_str = str(length_tweet) + "|" + str(num_words) + "|" + str(num_unique_chars) + "|" \
-                    + str(num_hashtags) + "|" + str(retweet_cnt) + "|" + str(num_swear_words) + "|" \
-                    + str(num_at_emotions)
+    topic_counter += 1
+    print "topic #", topic_counter
+    count_fetched = 0
+    max_id = -1
+    while count_fetched < total_count:
+        row_num = 0
+        search_results = twitter_api.search.tweets(q=topic, count=count_per_search, max_id=max_id)
+        statuses = search_results["statuses"]
+        search_results_len = len(statuses)
+        while row_num < count_per_search:
+            status = statuses[row_num]
+            text = status["text"]
+            # Message based features
+            length_tweet = len(text)
+            num_words = len(text.split())
+            num_unique_chars = CommonUtil.count_unique_chars(text)
+            num_hashtags = text.count("#")
+            retweet_cnt = status["retweet_count"]
+            max_id = status["id"]
+            num_swear_words = CommonUtil.count_swear_words(text)
+            num_at_emotions = text.count("@")
+            tweet_str = str(length_tweet) + "|" + str(num_words) + "|" + str(num_unique_chars) + "|" \
+                        + str(num_hashtags) + "|" + str(retweet_cnt) + "|" + str(num_swear_words) + "|" \
+                        + str(num_at_emotions)
 
 
-        outfile.write(tweet_str)
-        outfile.write("\n")
-        row_num += 1
+            outfile.write(tweet_str)
+            outfile.write("\n")
+            row_num += 1
+        count_fetched += search_results_len
+        print "count_fetched", count_fetched
+    print "-------------------------------------------------"
 outfile.close()
 
 #read file
@@ -56,7 +67,7 @@ outfile.close()
 f = open("test.txt", "r")
 lineCnt = 0
 for line in f:
-    print line
+    #print line
     lineCnt+=1
 
 print lineCnt
