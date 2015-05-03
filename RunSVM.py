99e9ea293c4c49e5f96ce89fe278d27837b10377
Bikram Kashyap <kashyap.bikram@gmail.com>
2015-05-03 19:36:33 -0400

    bug fixing

--- RunSVM.py
+++ RunSVM.py
@@ -11,6 +11,8 @@ from django.utils.encoding import smart_str, smart_unicode
 
 
 
+
+
 def preprocess():
     n_features = 14
     n_sample = 5000
@@ -53,6 +55,7 @@ def fetch_new_tweets():
     WORLD_WOE_ID = 1
 
     tweet_list = []
+    tweet_text_list = []
 
     us_trends = twitter_api.trends.place(_id=WORLD_WOE_ID)
 
@@ -112,47 +115,44 @@ def fetch_new_tweets():
                 if user_url:
                     t.has_url = 1
                 # Create tweet characteristics to write to file
-                tweet_str =  smart_str(text) + "|" + str(t.length_tweet) + "|" + str(t.num_words) + "|" + str(t.num_unique_chars) + "|" \
+                tweet_str =  str(t.length_tweet) + "|" + str(t.num_words) + "|" + str(t.num_unique_chars) + "|" \
                             + str(t.num_hashtags) + "|" + str(t.retweet_cnt) + "|" +  str(t.num_swear_words) + "|" \
                             + str(t.num_at_emotions) + "|" \
                             + str(t.registration_age) + "|" + str(t.num_followers) + "|" + str(t.num_followee) + "|" \
                             + str(t.is_verified) + "|" + str(t.len_desc) + "|" + str(t.len_screen_name) + "|" \
                             + str(t.has_url)
                 tweet_list.append(tweet_str)
+                tweet_text_list.append(smart_str(text))
 
                 row_num += 1
             count_fetched += search_results_len
-    return tweet_list
+    return tweet_list , tweet_text_list
 
 
-def predict_label(clf, tweet_list):
+def predict_label(clf, tweet_attr_list,tweet_text_list):
     n_features = 14
     row_index = 0
-    n_sample = len(tweet_list)
-    test_data = np.zeros((n_sample, n_features+1), dtype=np.int)
-    tweet_text_list = []
+    n_sample = len(tweet_attr_list)
+    test_data = np.zeros((n_sample, n_features), dtype=np.int)
     outfile = open("tweet_prediction.txt", "w")
-    for tweet in tweet_list:
+    for tweet in tweet_attr_list:
         row_data = tweet.split("|")
-        tweet_text = row_data[0]
-        tweet_text_list.append(tweet_text)
-        row_data = row_data[1:]
         column_index = 0
         for data in row_data:
             test_data[row_index][column_index] = data
             column_index += 1
         row_index += 1
-    print "Actual Tweet Size", len(tweet_text_list)
     predicted_label = clf.predict(test_data)
     pl_index = 0
     for tweet_text in tweet_text_list:
-        tweet_text = tweet_text + "|" + predict_label[pl_index]
+        tweet_text = tweet_text + "|" + str(predicted_label[pl_index])
         pl_index += 1
         outfile.write(tweet_text)
+        outfile.write("\n")
     outfile.close()
 
 
 train_data, train_label = preprocess()
 clf = run_svm(train_data, train_label)
-tweet_list = fetch_new_tweets()
-predict_label(clf,tweet_list)
+tweet_attr_list,tweet_test_list = fetch_new_tweets()
+predict_label(clf,tweet_attr_list,tweet_test_list)
